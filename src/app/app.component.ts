83498316670ab40657818fbc37378f5d7009e72c
River <revir.qing@gmail.com>
2025-04-27 19:00:25 +0200

    pdf reader available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PdfViewerComponent } from './pdf-viewer/pdf-viewer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PdfViewerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'pnl-pdf-reader';
---
+++ src/app/pdf-viewer/pdf-viewer.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import {
+  NgxExtendedPdfViewerModule,
+  NgxExtendedPdfViewerService,
+  pdfDefaultOptions,
+} from 'ngx-extended-pdf-viewer';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pdf-viewer',
+  templateUrl: './pdf-viewer.component.html',
+  styleUrls: ['./pdf-viewer.component.css'],
+  standalone: true,
+  imports: [NgxExtendedPdfViewerModule, CommonModule],
+  providers: [NgxExtendedPdfViewerService],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+// const testPdf = '/assets/pdfs/Bootstrap-vs-Material-Design-vs-Prime-vs-Tailwind.pdf';
+export class PdfViewerComponent {
+  pdfSrc: string | ArrayBuffer | null = null;
+  isDragging = false;
+  constructor(private pdfService: NgxExtendedPdfViewerService) {
+    /* More likely than not you don't need to tweak the pdfDefaultOptions.
+       They are a collecton of less frequently used options.
+       To illustrate how they're used, here are two example settings: */
+    // pdfDefaultOptions.doubleTapZoomFactor = '150%'; // The default value is '200%'
+    // pdfDefaultOptions.maxCanvasPixels = 4096 * 4096 * 5; // The default value is 4096 * 4096 pixels,
+    // but most devices support much higher resolutions.
+    // Increasing this setting allows your users to use higher zoom factors,
+    // trading image quality for performance.
+  }
+  onFileSelect(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      console.log('Selected file:', input.files[0]);
+      this.readFile(input.files[0]);
+    }
+  }
+
+  onFileDrop(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragging = false;
+    if (event.dataTransfer && event.dataTransfer.files.length > 0) {
+      console.log('Dropped file:', event.dataTransfer.files[0]);
+      this.readFile(event.dataTransfer.files[0]);
+    }
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragging = true;
+  }
+
+  onDragLeave(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragging = false;
+  }
+
+  private readFile(file: File): void {
+    if (file.type === 'application/pdf') {
+      const reader = new FileReader();
+      reader.onload = () => {
+        console.log('File read successfully:', reader.result);
+        this.pdfSrc = reader.result;
+      };
+      reader.onerror = (error) => {
+        console.error('Error reading file:', error);
+        alert('Error reading file. Please try again.');
+      };
+      // reading progress
+      reader.onprogress = (event) => {
+        if (event.lengthComputable) {
+          const percentComplete = (event.loaded / event.total) * 100;
+          console.log(`File reading progress: ${percentComplete}%`);
+        }
+      };
+      reader.readAsArrayBuffer(file);
+    } else {
+      alert('Please upload a valid PDF file.');
+    }
+  }
+}
